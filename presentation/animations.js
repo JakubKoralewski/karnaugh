47eec1d93af7e172a488db4f6e552606a4f90488
Jakub Koralewski <contact@jcubed.me>
2020-05-14 09:41:18 +0200

    move reusable animations into own file

---
+++ presentation/animations.js
@@ -0,0 +1,65 @@
+import React, {useRef} from "react"
+import {motion} from "framer-motion"
+
+export const Animation = React.forwardRef((props, ref) => {
+    let child = props.children
+    let type = child.type
+    let SomeTag = motion[type]
+
+    // https://stackoverflow.com/questions/35152522/react-transferring-props-except-one
+    const {onMount, onUnMount, ...otherProps} = props
+    if(onMount) {
+        useEffect(onMount, [])
+    }
+    return (
+        <SomeTag
+            {...otherProps}
+            ref={ref}
+        >
+            {child.props.children}
+        </SomeTag>
+    )
+})
+
+export const ScrollIntoViewAnimation = React.forwardRef((props, ref) => {
+    console.log("scroll into view animation props refs: ", props, ref)
+    let scrollObj
+    if (!ref) {
+        ref = useRef()
+        scrollObj = {
+            onMount: () => {
+                ref.current.scrollIntoView()
+                return () => ref.current.scrollIntoView()
+            },
+        }
+    } else {
+        scrollObj = {
+            onAnimationStart: () => {
+                console.log("ref on animation start", ref)
+                ref.current.scrollIntoView()
+            }
+        }
+    }
+    return (
+        <Animation
+            ref={ref}
+            {...scrollObj}
+            {...props}
+        >
+            {props.children}
+        </Animation>
+    )
+})
+
+export function BasicAnimation(props) {
+    let ref = useRef()
+    return (
+        <ScrollIntoViewAnimation
+            initial={{y: 100, opacity: 0}}
+            animate={{y: 0, opacity: 1}}
+            ref={ref}
+        >
+            {props.children}
+        </ScrollIntoViewAnimation>
+    )
+}
--- presentation/slides/intro.js
+++ presentation/slides/intro.js
@@ -1,70 +1,7 @@
 import styles from './slides.module.scss'
-import {motion} from "framer-motion"
 import InputFormula from "../../components/input_formula"
 import React, {useRef, useEffect} from "react"
-
-const Animation = React.forwardRef((props, ref) => {
-    let child = props.children
-    let type = child.type
-    let SomeTag = motion[type]
-
-    // https://stackoverflow.com/questions/35152522/react-transferring-props-except-one
-    const {onMount, onUnMount, ...otherProps} = props
-    if(onMount) {
-        useEffect(onMount, [])
-    }
-    return (
-        <SomeTag
-            {...otherProps}
-            ref={ref}
-        >
-            {child.props.children}
-        </SomeTag>
-    )
-})
-
-const ScrollIntoViewAnimation = React.forwardRef((props, ref) => {
-    console.log("scroll into view animation props refs: ", props, ref)
-    let scrollObj
-    if (!ref) {
-        ref = useRef()
-        scrollObj = {
-            onMount: () => {
-                ref.current.scrollIntoView()
-                return () => ref.current.scrollIntoView()
-            },
-        }
-    } else {
-        scrollObj = {
-            onAnimationStart: () => {
-                console.log("ref on animation start", ref)
-                ref.current.scrollIntoView()
-            }
-        }
-    }
-    return (
-        <Animation
-            ref={ref}
-            {...scrollObj}
-            {...props}
-        >
-            {props.children}
-        </Animation>
-    )
-})
-
-function BasicAnimation(props) {
-    let ref = useRef()
-    return (
-        <ScrollIntoViewAnimation
-            initial={{y: 100, opacity: 0}}
-            animate={{y: 0, opacity: 1}}
-            ref={ref}
-        >
-            {props.children}
-        </ScrollIntoViewAnimation>
-    )
-}
+import {BasicAnimation, ScrollIntoViewAnimation} from "../animations"
 
 
 function Intro(props) {
@@ -81,16 +18,6 @@ function Intro(props) {
                         Hello there!
                     </h2>
                 </BasicAnimation>
-                /*
-                                <motion.h2
-                                initial={{y: 100, opacity: 0}}
-                                animate={{y: 0, opacity: 1}}
-                                ref={ref}
-                                onAnimationStart={(x) => ref.current.scrollIntoView()}
-                                >
-                                Hello there!
-                                </motion.h2>
-                */
             }
             {
                 props.step >= 2 &&
