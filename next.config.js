c132a9c262277f846089387cf325b2672286c7c8
Jakub Koralewski <contact@jcubed.me>
2020-05-14 12:27:07 +0200

    netlify: static folder env variable check

--- next.config.js
+++ next.config.js
@@ -1,15 +1,26 @@
 const {PHASE_PRODUCTION_BUILD} = require("next/constants")
 
-const staticFolder = '/karnaugh'
-
-module.exports = (phase, {}) => ({
+module.exports = (phase, {}) => {
+    //enable both gh pages (/karnaugh) and netlify with (/)
+    console.log("KARNAUGH_PATH: ", process.env.KARNAUGH_PATH)
+    console.log("ASSET_PREFIX: ", process.env.ASSET_PREFIX)
+    let staticFolder = process.env.KARNAUGH_PATH !== undefined ? process.env.KARNAUGH_PATH : '/karnaugh'
+    console.log("STATIC_FOLDER: ", staticFolder)
+    let assetPrefix
+    if(process.env.ASSET_PREFIX !== undefined) {
+        assetPrefix = process.env.ASSET_PREFIX
+    } else {
+        assetPrefix = phase === PHASE_PRODUCTION_BUILD ? `https://jcubed.me${staticFolder}/` : ''
+    }
+    console.log("ASSET_PREFIX: ", assetPrefix)
+    return {
     env: {
         prod: PHASE_PRODUCTION_BUILD === phase,
         // Will be available on both server and client
-        staticFolder,
+        staticFolder
     },
     devIndicators: {
         autoPrerender: false,
     },
-    assetPrefix: phase === PHASE_PRODUCTION_BUILD ? `https://jcubed.me${staticFolder}/` : '',
-})
+    assetPrefix
+}}
