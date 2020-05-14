430a5608cbc03b6d337acdeb9f9f6d636a3b6484
Jakub Koralewski <contact@jcubed.me>
2020-05-14 09:45:53 +0200

    fix animations.js no {useEffects}

--- presentation/animations.js
+++ presentation/animations.js
@@ -1,4 +1,4 @@
-import React, {useRef} from "react"
+import React, {useRef, useEffect} from "react"
 import {motion} from "framer-motion"
 
 export const Animation = React.forwardRef((props, ref) => {
