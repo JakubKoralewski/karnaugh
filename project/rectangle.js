1434ccc3cb215419dd5cbd44a89a931fe81c10b3
Jakub Koralewski <contact@jcubed.me>
2020-06-03 15:08:16 +0200

    Fixes #31

--- project/rectangle.js
+++ project/rectangle.js
@@ -64,7 +64,6 @@ export class Rectangles {
     constructor({rectangles: arrays, rowLength}) {
         this.rowLength = rowLength
         this.colors = new Colors()
-        this.usedColors = new Set()
         this.rectangles = arrays.map(r => new Rectangle(r, this.colors.getRandom(), rowLength))
         this.map = this.generateMap()
     }
@@ -108,44 +107,55 @@ class Colors {
     usedColors = new Set();
 
     names = {
+        lightblue: "rgb(173,216,230)",
         aqua: "rgb(65,180,180)",
         blue: "rgb(0,0,255)",
-        brown: "rgb(165,58,42)",
         darkcyan: "rgb(0,139,139)",
         darkgreen: "rgb(0,100,0)",
-        darkkhaki: "rgb(189,183,107)",
-        darkmagenta: "rgb(139,0,139)",
-        darkolivegreen: "rgb(85,107,47)",
-        darkorange: "rgb(255,140,0)",
-        darkred: "rgb(139,0,0)",
-        darksalmon: "rgb(233,150,122)",
-        darkviolet: "rgb(148,0,211)",
-        fuchsia: "rgb(255,0,255)",
-        gold: "rgb(255,215,0)",
         green: "rgb(0,128,0)",
+        darkkhaki: "rgb(189,183,107)",
         khaki: "rgb(240,230,140)",
-        lightblue: "rgb(173,216,230)",
+        yellow: "rgb(255,255,0)",
         lime: "rgb(0,255,0)",
-        olive: "rgb(128,128,0)",
+        gold: "rgb(255,215,0)",
+        red: "rgb(255,0,0)",
+        darkred: "rgb(139,0,0)",
+        brown: "rgb(165,58,42)",
+        darkorange: "rgb(255,140,0)",
+        darksalmon: "rgb(233,150,122)",
         orange: "rgb(255,165,0)",
         pink: "rgb(255,192,203)",
+        darkviolet: "rgb(148,0,211)",
+        fuchsia: "rgb(255,0,255)",
         purple: "rgb(128,0,128)",
-        red: "rgb(255,0,0)",
-        yellow: "rgb(255,255,0)"
+        darkmagenta: "rgb(139,0,139)",
+        black: "rgb(0,0,0)",
+        gray: "rgb(100, 100, 100)",
     };
 
     getRandom() {
+        // names of all colors
         const colors = Object.keys(this.names)
+
+        // generate possibly repeated random number
         const firstRandomColorNumber = Math.floor(Math.random() * colors.length)
+
+        // assign to new variable to be able to check if all colors exhausted
         let randomColorNumber = firstRandomColorNumber
+
+        // while this color is repeated, do
         while (this.usedColors.has(colors[randomColorNumber])) {
-            randomColorNumber++
+            // Try other color, sorted by similarity so should be different
+            randomColorNumber += colors.length/2
+            // Make sure it's not larger than available colors
             randomColorNumber %= colors.length
             if (randomColorNumber === firstRandomColorNumber) {
+                // Exhausted all possibilities, tried all colors
                 console.warn("Reusing old colors")
                 this.usedColors = new Set()
             }
         }
+        this.usedColors.add(colors[randomColorNumber])
         return this.names[colors[randomColorNumber]];
     }
 }
