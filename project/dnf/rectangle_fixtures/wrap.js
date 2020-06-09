99b6e4644df5c22503353d41239582e0b3297818
Jakub Koralewski <contact@jcubed.me>
2020-06-09 16:24:42 +0200

    new simpler horizontal bottom to top wrapping test

--- project/dnf/rectangle_fixtures/wrap.js
+++ project/dnf/rectangle_fixtures/wrap.js
@@ -4,6 +4,18 @@ const otherFixturesAbstracted = [
     {
         input: [
             [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+        ],
+        output: [
+            [0, 1, 2, 3, 12, 13, 14, 15]
+        ],
+        description: `Wrapping horizontally bottom to top`
+    },
+    {
+        input: [
+            [1, 1, 1, 1],
             [1, 1, 1, 1],
             [1, 1, 1, 1],
             [1, 1, 1, 1],
