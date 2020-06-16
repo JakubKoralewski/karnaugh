e102d625d2ed6eb8689beefa8fa3a2159f18c434
Jakub Koralewski <contact@jcubed.me>
2020-06-16 08:59:55 +0200

    Add new examples

--- components/input_formula/dropdown_options.js
+++ components/input_formula/dropdown_options.js
@@ -11,9 +11,22 @@ export default [
     {
         label: "Wrapping",
         group: [
+            "p & q & r & (s & ~s) || ((~r & ~s) || (r & ~s))",
+            "p & q & r & (s & ~s) || ((~p & ~q) || (p & ~q))",
+            "p & q & (r & ~r) & s || (~s & ~q)",
+            "p & q & r & (s & ~s) || (~q & ~ t)"
+        ]
+    },
+    {
+        label: "Problems",
+        group: [
             "(~p || ~q || r) || (s & ~s & t)",
+        ]
+    },
+    {
+        label: "Interesting examples",
+        group: [
             "p <-> q <-> r <-> s <-> t",
-            "(p & q & r & s & ~p) || (~q & ~ t)"
         ]
     }
 ]
